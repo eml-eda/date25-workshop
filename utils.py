0d52570201d1a0716c87e379064ad21e3a650672
Petrolo Vincenzo <s295043@studenti.polito.it>
2025-04-04 16:47:52 +0200

    chore : better plot

--- utils.py
+++ utils.py
@@ -28,33 +28,33 @@ def plot_results():
     for n in nodes:
         total_arcane = arcane_values[n] + cpu_easy_values[n]
         arcane_bars = ax.bar(
-            n - bar_width / 2,
+            n - bar_width/2 / 2,
             arcane_values[n]/total_arcane,
-            bar_width,
-            color="indianred",
+            bar_width/2,
+            color="#009CA6",
         )
 
         cpu_arcane_bars = ax.bar(
-            n - bar_width / 2,
+            n - bar_width/2 / 2,
             cpu_easy_values[n]/total_arcane,
-            bar_width,
+            bar_width/2,
             bottom=arcane_values[n]/total_arcane,
-            color="maroon",
+            color="#840D35",
         )
 
         # Plot CPU-only bars
         cpu_bars = ax.bar(
-            n + bar_width / 2,
+            n + bar_width/2 / 2,
             cpu_only_values[n]/total_arcane,
-            bar_width,
-            color="navajowhite",
+            bar_width/2,
+            color="#434343",
         )
 
         ax.text(
-            n + bar_width / 2 - 0.1,
-            (cpu_only_values[n]/total_arcane)+0.15,
+            n + bar_width/2 / 2 - 0.2,
+            (cpu_only_values[n]/total_arcane)+0.3,
             f'{cpu_only_values[n]/total_arcane:.2f}x',
-            ha='left', va='center', color='darkgoldenrod',
+            ha='left', va='center', color='black',
             fontweight='bold',
             fontsize=12,
         )
@@ -63,20 +63,20 @@ def plot_results():
     total_cpu_only = sum(cpu_only_values)
 
     arcane_bars = ax.bar(
-        len(nodes) + bar_width / 2,
-        total_cpu_only/total_arcane,
-        bar_width,
-        color="navajowhite",
+        len(nodes) - bar_width/2 / 2,
+        1,
+        bar_width/2,
+        color="#009CA6",
     )
     cpu_bars = ax.bar(
-        len(nodes) - bar_width / 2,
-        1,
-        bar_width,
-        color="indianred",
+        len(nodes) + bar_width/2 / 2,
+        total_cpu_only/total_arcane,
+        bar_width/2,
+        color="#434343",
     )
     # Add an arrow and text to show the improvement
     improvement = total_cpu_only / total_arcane
-    x_pos = len(nodes) - bar_width / 2
+    x_pos = len(nodes) - bar_width/2 / 2
     ax.annotate(
         '',
         xy=(x_pos, 1), 
@@ -86,7 +86,7 @@ def plot_results():
     )
     ax.text(
         x_pos - 0.1,
-        (total_cpu_only/total_arcane)+0.1,
+        (total_cpu_only/total_arcane)+0.3,
         f'{improvement:.2f}x',
         ha='left', va='center', color='firebrick',
         fontweight='bold',
@@ -98,12 +98,29 @@ def plot_results():
     ax.set_title("ARCANE vs CPU-Only Profiling per Node")
     ax.set_xticks([n for n in nodes]+[len(nodes)])
     ax.set_xticklabels([f"Node {n}" for n in nodes]+["Total"])
-    plt.legend(handles=[
-        plt.Line2D([0], [0], color="indianred", label='ARCANE NMC'),
-        plt.Line2D([0], [0], color="maroon", label='ARCANE CPU'),
-        plt.Line2D([0], [0], color="navajowhite", label='CPU-Only'),
-    ]
+    # plt.legend(handles=[
+    #     plt.Line2D([0], [0], color="#009CA6", label='ARCANE NMC'),
+    #     plt.Line2D([0], [0], color="#840D35", label='ARCANE CPU'),
+    #     plt.Line2D([0], [0], color="#434343", label='CPU-Only'),
+    # ]
+    # )
+    # Draw a legend in top center with rectangles rather than lines
+    ax.legend(
+        [
+            plt.Rectangle((0, 0), 1, 1, fc="#009CA6", edgecolor = 'none'),
+            plt.Rectangle((0, 0), 1, 1, fc="#840D35", edgecolor = 'none'),
+            plt.Rectangle((0, 0), 1, 1, fc="#434343", edgecolor = 'none'),
+        ],
+        ['ARCANE NMC', 'ARCANE CPU', 'CPU-Only'],
+        loc='upper center',
+        bbox_to_anchor=(0.5, 1),
+        ncol=3,
+        fontsize=11,
     )
+
+    ax.set_ylim(0, 7)
+    # Draw a line of 1 dashed
+    # ax.axhline(1, color='black', linestyle='--')
     # Show the plot
     # plt.tight_layout()
     plt.show()
